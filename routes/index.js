2bf2ac2eb9236e7c06d1e4013e09fbeae1ac84ed
Brett Richard Nitschke <bnitschk@mail.sfsu.edu>
2018-01-30 16:20:43 -0800

    first

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var auth = require('../controllers/AuthController');
+
+//auth routes
+router.get('/', auth.home);
+
+router.get('/register', auth.register);
+
+router.post('/register', auth.doRegister);
+
+router.get('/login', auth.login);
+
+router.post('/login', auth.doLogin);
+
+router.get('/logout', auth.logout);
+
+module.exports = router;
